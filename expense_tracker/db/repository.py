a05f20c75168a6f73e6b42995dbff1279a1cca1b
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-09-04 17:38:12 +0530

    updating and deleting expense

--- expense_tracker/db/repository.py
+++ expense_tracker/db/repository.py
@@ -38,3 +38,43 @@ def create_expense(session , amount , category_id , date , note=None):
     print(f"new expense of {amount} added")
 
     return new_expense
+
+
+
+
+def delete_expense(session , expense_id):
+
+    expense = session.query(Expense).filter_by(id=expense_id).first()
+
+    if expense is None:
+        print("no such expense exists")
+        return None
+
+    session.delete(expense)
+    return expense
+
+
+
+def update_expense(session , expense_id , amount=None , category_id=None , note=None , date=None):
+
+    expense = session.query(Expense).filter_by(id=expense_id).first()
+    
+    if expense is None:
+        print("no such expense exists")
+        return None
+
+    if amount is not None:
+        expense.amount = amount
+    if category_id is not None:
+        expense.category_id = category_id
+    if note is not None:
+        expense.note = note
+    if date is not None:
+        expense.date = date
+ 
+    session.flush()
+    return expense
+
+
+
+    
