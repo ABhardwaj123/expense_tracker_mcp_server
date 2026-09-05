c5a5c4e527b5b58366d3201feb542f90242602f1
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-09-05 17:28:21 +0530

    getting summary and expenses along with setting budget

--- expense_tracker/services/expense_service.py
+++ expense_tracker/services/expense_service.py
@@ -82,3 +82,78 @@ def delete_expense(expense_id):
     session.commit()
     session.close()
     return result
+
+
+
+def get_expenses(limit=10 , category=None , start_date=None , end_date=None):
+    session = SessionLocal()
+
+    expenses = repository.get_expenses(session , limit , category , start_date , end_date)
+
+    result = []
+
+    for expense in expenses:
+        result.append({
+            "id": expense.id,
+            "amount": expense.amount,
+            "category": expense.category.name,
+            "note": expense.note,
+            "date": expense.date,
+        })
+
+    session.close()
+    return result
+
+
+
+
+def get_summary(start_date=None , end_date=None , group_by=None):
+    expenses = get_expenses(limit=10000 , start_date=start_date , end_date=end_date)
+
+    if group_by:
+
+        totals_by_category = {}
+
+        for expense in expenses:
+            category_name = expense["category"]
+            amount = expense["amount"]
+
+            if category_name not in totals_by_category:
+                totals_by_category[category_name] = 0
+
+            totals_by_category[category_name] += amount
+
+        return totals_by_category
+
+    else:
+        total = sum(expense["amount"] for expense in expenses)
+        return {"total": total}
+
+
+
+
+def set_budget(category=None, monthly_budget=None, month=None, year=None):
+    session = SessionLocal()
+
+    if category is not None:
+        category_id = repository.create_category(session, category)
+    else:
+        category_id = None
+
+    if month is None or year is None:
+        today = date.today()
+        month = today.month
+        year = today.year
+
+    budget = repository.set_budget(session, category_id, monthly_budget, month, year)
+
+    result = {
+        "category": category,
+        "monthly_budget": budget.monthly_budget,
+        "month": budget.month,
+        "year": budget.year,
+    }
+
+    session.commit()
+    session.close()
+    return result
